b1d73c9b66e036dbf40b849486303495a9f7f17f
mattbrailsford <me@mattbrailsford.com>
2013-09-29 20:40:38 +0100

    Initial comit

---
+++ Src/OAuthProxy.Web/Redirect.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace OAuthProxy.Web
+{
+    public partial class Redirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Grab querystring parameters
+                var parameters = Request.QueryString;
+
+                // Parse the url param
+                var encodedUrl = parameters["url"];
+                var bytes = HttpServerUtility.UrlTokenDecode(encodedUrl);
+                var url = new UTF8Encoding().GetString(bytes);
+
+                // Make sure url starts with http
+                if(!Regex.IsMatch(url, "^https?://", RegexOptions.IgnoreCase))
+                    throw new ArgumentException("Invalid url", "url");
+
+                // Remove the url param from parms collection
+                parameters.Remove("url");
+
+                // Construct final redirect url
+                var redirectUrl = url +
+                    (url.IndexOf("?") >= 0 ? "&" : "?") +
+                    ConstructQueryString(parameters);
+
+                // Perform redirect
+                Response.StatusCode = 301;
+                Response.Status = "301 Moved Permanently";
+                Response.AddHeader("Location", redirectUrl);
+
+            }
+            catch (Exception)
+            {
+                // Something went wrong so just return bad request
+                Response.StatusCode = 400;
+                Response.Status = "400 Bad Request";
+            }
+        }
+
+        public static string ConstructQueryString(NameValueCollection parameters)
+        {
+            return string.Join("&", 
+                parameters.AllKeys.Select(name => 
+                    string.Concat(name, "=", HttpUtility.UrlEncode(parameters[name])))
+                    .ToArray());
+        }
+    }
+}
